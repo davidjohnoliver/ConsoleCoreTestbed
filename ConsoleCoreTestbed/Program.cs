8d867056e085e270e92532fafcb5979ed8c20da1
David Oliver <david.oliver@nventive.com>
2019-10-30 20:51:40 -0400

    Add project files.

---
+++ ConsoleCoreTestbed/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleCoreTestbed
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var path = System.IO.Directory.GetCurrentDirectory();
+			var root = Path.GetPathRoot(path);
+			;
+		}
+	}
+}
